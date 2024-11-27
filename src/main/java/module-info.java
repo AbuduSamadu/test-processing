ee12237b63ca7c4d213a04d625e146a1e0d95f30
Abudu Samadu <abudu.samadu@amalitech.com>
2024-11-27 06:37:28 +0000

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module abudu.test.testprocessingtool {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens abudu.test.testprocessingtool to javafx.fxml;
+    exports abudu.test.testprocessingtool;
+}
